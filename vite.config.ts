15d100a11a632a7b8128f47e8063a401a693bec9
Fissya <fissyaneet@gmail.com>
2025-05-13 17:14:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/FEFO9th-V3/client',
+  base: '/FEFO9th-V3/', // Replace <repo-name> with your GitHub repository name
   plugins: [
     react(),
     runtimeErrorOverlay(),
