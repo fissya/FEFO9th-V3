10342fbf9f3a3117384efa62bae321bbe5f2cdb9
Fissya <fissyaneet@gmail.com>
2025-05-13 16:58:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/FEFO9th-V3/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
