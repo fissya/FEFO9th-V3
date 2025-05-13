9caac2633482cfcae0272df9aa8d8f9480f39c80
Fissya <fissyaneet@gmail.com>
2025-05-13 17:07:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/FEFO9th-V3/',
+  base: '/FEFO9th-V3/client',
   plugins: [
     react(),
     runtimeErrorOverlay(),
